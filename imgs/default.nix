1079cd70a738bf83f131e6022aa5834ed3ce9d85
Robert James Hernandez <rob@sarcasticadmin.com>
2020-09-23 23:55:46 +0000

    derp in helmsman

--- imgs/default.nix
+++ imgs/default.nix
@@ -6,7 +6,7 @@ rec {
   };
 
   helmsman-aws = pkgs.callPackage ./helmsman-aws {
-    pkgs = import ../pin { snapshot = "nixos-20-03_0"; };
+    pkgs = import ../pin { snapshot = "nixpkgs-unstable_0"; };
   };
 
   magic-wormhole-mailbox = pkgs.callPackage ./magic-wormhole-mailbox {
