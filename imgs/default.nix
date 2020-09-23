e0d1490bd4b5d76fd90293e24ceece29f54003d0
Robert James Hernandez <rob@sarcasticadmin.com>
2020-09-23 23:30:33 +0000

    moving pkgs up to default imgs level

--- imgs/default.nix
+++ imgs/default.nix
@@ -1,9 +1,13 @@
 { pkgs }:
 
 rec {
-  awsutils = pkgs.callPackage ./awsutils { };
+  awsutils = pkgs.callPackage ./awsutils {
+    pkgs = import ../pin { snapshot = "nixos-20-03_0"; };
+  };
 
-  helmsman-aws = pkgs.callPackage ./helmsman-aws { };
+  helmsman-aws = pkgs.callPackage ./helmsman-aws {
+    pkgs = import ../pin { snapshot = "nixos-20-03_0"; };
+  };
 
   magic-wormhole-mailbox = pkgs.callPackage ./magic-wormhole-mailbox { };
 
